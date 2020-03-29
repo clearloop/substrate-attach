6d90adbf62aa544894c5723688608df316083129
clearloop <udtrokia@gmail.com>
2020-03-29 22:58:45 +0800

    add: js template

---
+++ template/substrate-attach.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+node -i -e '
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+const customizeType = require("./types.json");
+
+async function setPolkadotJs() {
+    return await ApiPromise.create({
+    	types: customizeType,
+    	provder: new WsProvider("ws://0.0.0.0:9944"),
+    }).catch(e => console.error);
+}
+
+// expose to repl
+const api = setPolkadotJs();
+'
